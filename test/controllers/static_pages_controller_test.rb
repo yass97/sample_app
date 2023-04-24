e2e1e3e779be9f5db5058c889d699ca67a546eda
Yasunori Matsuda <yasunori-matsuda@cookpad.com>
2023-04-24 12:46:38 +0900

    Fix home test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,7 +7,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
