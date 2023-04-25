ecf5f8e84fa521921548f647c2f9da03f4faeaa1
Yasunori Matsuda <yasunori-matsuda@cookpad.com>
2023-04-25 15:22:38 +0900

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    user = {
+      name: "",
+      email: "user@invalid",
+      password: "foo",
+      password_confirmation: "bar"
+    }
+
+    assert_no_difference "User.count" do post users_path, params: { user: user } end
+    assert_template "users/new"
+    assert_select "div#error_explanation"
+    assert_select "div.alert"
+  end
+
+  test "valid signup information" do
+    get signup_path
+    user = { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" }
+    assert_difference "User.count", 1 do post users_path, params: { user: user }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.empty?
+  end
+end
