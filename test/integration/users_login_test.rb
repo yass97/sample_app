c7e5e80f06bc8af31b89517a26248b43c9d3b656
yass97 <mongolyassan@gmail.com>
2023-04-26 14:52:30 +0900

    To リスト 9.24:log_in_asヘルパーを追加する

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,42 +1,43 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-
   def setup
     @user = users(:michael)
   end
 
-  test "login with invalid information" do
+  test 'login with invalid information' do
     get login_path
-    post login_path, params: { session: { email: @user.email, password: "password" } }
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
     assert_redirected_to @user
     # 実際にリダイレクト先のページへ遷移する
     follow_redirect!
-    assert_template "users/show"
+    assert_template 'users/show'
     # ログインへのパスを持ったリンクが表示されなくなったことを確認している
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
   end
 
-  test "login with valid information followed by logout" do
+  test 'login with valid information followed by logout' do
     get login_path
-    post login_path, params: { session: { email: @user.email, password: "password" } }
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
 
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
-    assert_template "users/show"
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
 
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
+    delete logout_path
     follow_redirect!
-    assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path, count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
-require "minitest/reporters"
+require 'minitest/reporters'
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
@@ -11,5 +11,17 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me } }
   end
 end
