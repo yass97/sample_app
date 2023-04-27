cbdf81e14111262d9fd60a651e29f3e14f971c1d
Yasunori Matsuda <yasunori-matsuda@cookpad.com>
2023-04-28 08:16:32 +0900

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -16,5 +20,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: %i[edit]
+  resources :password_resets, only: %i[new create edit update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
